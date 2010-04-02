4048ee65f0e4401cf530a892bcfd7bfdc03e9283
webtechnick <nick@webtechnick.com>
2010-04-02 02:26:56 +0000

    Initial import.

---
+++ vendors/rails_reporter.php
@@ -0,0 +1,199 @@
+<?php
+/**
+ * Rails test reporter.
+ * PHP version 4 and 5
+ * 
+ * @author        Nick Baker
+ * @copyright     2010 webtechnick
+ * @link          http://www.webtechnick.com
+ * @license       MIT
+ */
+	if (version_compare(PHP_VERSION, '4.4.4', '<=') ||
+		PHP_SAPI == 'cgi') {
+		define('STDOUT', fopen('php://stdout', 'w'));
+		define('STDERR', fopen('php://stderr', 'w'));
+		register_shutdown_function(create_function('', 'fclose(STDOUT); fclose(STDERR); return true;'));
+	}
+
+App::import('Vendor', 'simpletest' . DS . 'reporter');
+include_once CAKE . 'tests' . DS . 'lib'. DS . 'reporter' . DS . 'cake_base_reporter.php';
+
+/**
+ * Minimal command line test displayer. Writes fail details to STDERR. Returns 0
+ * to the shell if all tests pass, ST_FAILS_RETURN_CODE if any test fails.
+ *
+ * @package cake
+ * @subpackage cake.tests.libs.reporter
+ */
+class RailsReporter extends CakeBaseReporter {
+/**
+ * separator string for fail, error, exception, and skip messages.
+ *
+ * @var string
+ */
+	var $separator = '->';
+
+/**
+ * array of 'request' parameters
+ *
+ * @var array
+ */
+	var $params = array();
+
+/**
+* string of errors,exceptions,and anything else we want to output at the end of the tests
+*
+* @var string
+*/
+	var $buffer = null;
+
+/**
+ * Constructor
+ *
+ * @param string $separator 
+ * @param array $params 
+ * @return void
+ */
+	function RailsReporter($charset = 'utf-8', $params = array()) {
+		$this->CakeBaseReporter($charset, $params);
+	}
+
+	function setFailDetailSeparator($separator) {
+		$this->separator = $separator;
+	}
+
+/**
+ * Paint fail faildetail to STDERR.
+ *
+ * @param string $message Message of the fail.
+ * @return void
+ * @access public
+ */
+	function paintFail($message) {
+		parent::paintFail($message);
+		$message .= $this->_getBreadcrumb();
+		$this->buffer .= 'FAIL' . $this->separator . $message;
+		fwrite(STDERR, 'f');
+	}
+	
+/**
+ * Paint pass passdetail to STDOUT.
+ *
+ * @param string $message Message of the fail.
+ * @return void
+ * @access public
+ */	
+	function paintPass($message){
+		parent::paintPass($message);
+		if(isset($this->params['verbose']) && $this->params['verbose']) {
+			$message .= $this->_getBreadcrumb();
+			$this->buffer .= 'PASS' . $this->separator . $message;
+		}
+		fwrite(STDOUT, '.');
+	}
+
+/**
+ * Paint PHP errors to STDERR.
+ *
+ * @param string $message Message of the Error
+ * @return void
+ * @access public
+ */
+	function paintError($message) {
+		parent::paintError($message);
+		$message .= $this->_getBreadcrumb();
+		$this->buffer .= 'ERROR' . $this->separator . $message;
+		fwrite(STDERR, 'e');
+	}
+
+/**
+ * Paint exception faildetail to STDERR.
+ *
+ * @param string $message Message of the Error
+ * @return void
+ * @access public
+ */
+	function paintException($exception) {
+		parent::paintException($exception);
+		$message .= sprintf('Unexpected exception of type [%s] with message [%s] in [%s] line [%s]',
+			get_class($exception),
+			$exception->getMessage(),
+			$exception->getFile(),
+			$exception->getLine()
+		);
+		$message .= $this->_getBreadcrumb();
+		$this->buffer .= 'EXCEPTION' . $this->separator . $message;
+		fwrite(STDERR, 'E');
+	}
+
+/**
+ * Get the breadcrumb trail for the current test method/case
+ *
+ * @return string The string for the breadcrumb
+ */
+	function _getBreadcrumb() {
+		$breadcrumb = $this->getTestList();
+		array_shift($breadcrumb);
+		$out = "\n\tin " . implode("\n\tin ", array_reverse($breadcrumb));
+		$out .= "\n\n";
+		return $out;
+	}
+
+/**
+ * Paint a test skip message
+ *
+ * @param string $message The message of the skip
+ * @return void
+ */
+	function paintSkip($message) {
+		parent::paintSkip($message);
+		$this->buffer .= 'SKIP' . $this->separator . $message . "\n\n";
+		fwrite(STDOUT, 's');
+	}
+
+/**
+ * Paint a footer with test case name, timestamp, counts of fails and exceptions.
+ */
+	function paintFooter($test_name) {
+		$buffer = "\n" . $this->buffer;
+		$buffer .=  $this->getTestCaseProgress() . '/' . $this->getTestCaseCount() . ' test cases complete: ';
+
+		if (0 < ($this->getFailCount() + $this->getExceptionCount())) {
+			$buffer .= $this->getPassCount() . " passes";
+			if (0 < $this->getFailCount()) {
+				$buffer .= ", " . $this->getFailCount() . " fails";
+			}
+			if (0 < $this->getExceptionCount()) {
+				$buffer .= ", " . $this->getExceptionCount() . " exceptions";
+			}
+			$buffer .= ".\n";
+			$buffer .= $this->_timeStats();
+			fwrite(STDOUT, $buffer);
+		} else {
+			fwrite(STDOUT, $buffer . $this->getPassCount() . " passes.\n" . $this->_timeStats());
+		}
+
+		if (
+			isset($this->params['codeCoverage']) && 
+			$this->params['codeCoverage'] && 
+			class_exists('CodeCoverageManager')
+		) {
+			CodeCoverageManager::report();
+		}
+	}
+
+/**
+ * Get the time and memory stats for this test case/group
+ *
+ * @return string String content to display
+ * @access protected
+ */
+	function _timeStats() {
+		$out = 'Time taken by tests (in seconds): ' . $this->_timeDuration . "\n";
+		if (function_exists('memory_get_peak_usage')) {
+			$out .= 'Peak memory use: (in bytes): ' . number_format(memory_get_peak_usage()) . "\n";
+		}
+		return $out;
+	}
+}
+?>
---
+++ vendors/shells/rake.php
@@ -0,0 +1,114 @@
+<?php
+/**
+  * A rake like test reporter for CakePHP
+  * 
+  * This extends the built in testsuite shell and overwrites the cake_cli_reporter 
+  * for a rails+rake type feel.  Tests are run and feedback is presented to the user
+  * passed tests are represented as ".", errors, exceptions, and skips are buffered 
+  * output until all tests are complete.   Durring the tests constant feedback is given
+  * to the user.  Each pass/fail/etc.. during testing is represented in like so:
+  * 
+  * pass      -> .
+  * fail      -> f
+  * error     -> e
+  * exception -> E
+  * skip      -> s
+  *
+  * If the verbose option is given all passes are also buffered and outputted.
+  * 
+  * @author Nick Baker
+  * @license MIT
+  * @example cake rake app all verbose
+  * @example cake rake app case models/user cov verbose
+  */
+require_once(CAKE . 'console' . DS . 'libs'. DS . 'testsuite.php');
+
+App::import('Vendor', 'Rake.rake_reporter');
+class RakeShell extends TestSuiteShell {
+  /**
+    * Stores if the user wishes to receive verbose feedback
+    * 
+    * @var boolean
+    * @access public
+    */
+  var $verbose = false;
+  
+  
+  /**
+    * Parse the arguments given into the Shell object properties.
+    *
+    * @return void
+    * @access public
+    */
+  function parseArgs(){
+    parent::parseArgs();
+    
+    if(!empty($this->args)){
+      foreach($this->args as $arg){
+        if($arg == 'verbose'){
+          $this->verbose = true;
+        }
+      }
+    }
+  }
+
+  /**
+    * Help screen
+    */
+  function help(){
+    parent::help();
+    $this->out();
+		$this->out('Verbose Output: ');
+		$this->out("\n\nAppend 'verbose' to any of the above to see verbose output of tests");
+  }
+  
+  /**
+    * Executes the tests depending on our settings
+    *
+    * @return void
+    * @access private
+    */
+	function __run() {
+		$Reporter = new RakeReporter('utf-8', array(
+			'app' => $this->Manager->appTest,
+			'plugin' => $this->Manager->pluginTest,
+			'group' => ($this->type === 'group'),
+			'codeCoverage' => $this->doCoverage,
+			'verbose' => $this->verbose
+		));
+
+		if ($this->type == 'all') {
+			return $this->Manager->runAllTests($Reporter);
+		}
+
+		if ($this->doCoverage) {
+			if (!extension_loaded('xdebug')) {
+				$this->out(__('You must install Xdebug to use the CakePHP(tm) Code Coverage Analyzation. Download it from http://www.xdebug.org/docs/install', true));
+				$this->_stop(0);
+			}
+		}
+
+		if ($this->type == 'group') {
+			$ucFirstGroup = ucfirst($this->file);
+			if ($this->doCoverage) {
+				require_once CAKE . 'tests' . DS . 'lib' . DS . 'code_coverage_manager.php';
+				CodeCoverageManager::init($ucFirstGroup, $Reporter);
+				CodeCoverageManager::start();
+			}
+			$result = $this->Manager->runGroupTest($ucFirstGroup, $Reporter);
+			return $result;
+		}
+
+		$folder = $folder = $this->__findFolderByCategory($this->category);
+		$case = $this->__getFileName($folder, $this->isPluginTest);
+
+		if ($this->doCoverage) {
+			require_once CAKE . 'tests' . DS . 'lib' . DS . 'code_coverage_manager.php';
+			CodeCoverageManager::init($case, $Reporter);
+			CodeCoverageManager::start();
+		}
+		$result = $this->Manager->runTestCase($case, $Reporter);
+		return $result;
+	}
+}
+?>
